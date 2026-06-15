8a1b97b735c4e8d63c49d8229a3725b4b2c02438
Harsh <harshkr3062025@gmail.com>
2026-06-15 11:54:59 +0530

    added mail to the website

--- backend/controllers/inquiryController.js
+++ backend/controllers/inquiryController.js
@@ -1,8 +1,10 @@
 const Inquiry = require("../models/Inquiry");
+const sendInquiryEmail = require("../utils/sendEmails");
 
 const createInquiry = async (req, res) => {
   try {
     const inquiry = await Inquiry.create(req.body);
+    await sendInquiryEmail(inquiry);
     res.status(201).json(inquiry);
   } catch (error) {
     res.status(500).json({ message: error.message });
