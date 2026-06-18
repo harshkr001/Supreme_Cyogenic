fa20a6314e611a2c6a5ff3070f8cb64f9b79e9cf
Harsh <harshkr3062025@gmail.com>
2026-06-18 17:18:57 +0530

    Logout button created

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AdminDashboard() {
     const [inquiries, setInquiries] = useState([]);
 
     const [search, setSearch] = useState("");
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         fetch("http://localhost:5000/api/inquiries")
@@ -67,9 +70,36 @@ function AdminDashboard() {
     const resolvedCount = inquiries.filter(
         (item) => item.status === "Resolved"
     ).length;
+
+    const handleLogout = () => {
+        localStorage.removeItem("isAdmin");
+        navigate("/admin-login");
+    }
     return (
         <div style={{ padding: "20px" }}>
-            <h1>Admin Dashboard</h1>
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                }}
+            >
+                <h1>Admin Dashboard</h1>
+
+                <button
+                    onClick={handleLogout}
+                    style={{
+                        backgroundColor: "red",
+                        color: "white",
+                        border: "none",
+                        padding: "10px 20px",
+                        borderRadius: "5px",
+                        cursor: "pointer",
+                    }}
+                >
+                    Logout
+                </button>
+            </div>
 
             <div
                 style={{
